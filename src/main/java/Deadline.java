e87fc3e2e49bfc676496a541b5113282e93f6c7c
Shiverin <shiverinz@gmail.com>
2026-08-19 17:15:38 +0800

    Parse and format deadline dates

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /** Represents a task that must be completed by a deadline. */
 public class Deadline extends Task {
-    protected final String by;
+    private static final DateTimeFormatter DISPLAY_FORMAT = DateTimeFormatter.ofPattern("MMM d yyyy");
+    private final LocalDate by;
 
     /** Creates a deadline task. */
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
+    }
+
+    /** Returns the deadline in the storage input format. */
+    public String getStorageDate() {
+        return by.toString();
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DISPLAY_FORMAT) + ")";
     }
 }
