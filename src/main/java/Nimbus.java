80a61b5eb78f0f5f620871fee4398e21ebae7a07
Shiverin <shiverinz@gmail.com>
2026-08-19 17:11:07 +0800

    Add command echoing

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -7,7 +7,7 @@ public class Nimbus {
     private static final String DIVIDER = "____________________________________________________________";
 
     /**
-     * Greets the user and exits.
+     * Greets the user, echoes commands, and exits when the user enters {@code bye}.
      *
      * @param args Command-line arguments, which are not used.
      */
@@ -17,6 +17,14 @@ public class Nimbus {
         System.out.println("Hello! I'm Nimbus.");
         System.out.println("What can I do for you?");
         System.out.println(DIVIDER);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(command);
+            System.out.println(DIVIDER);
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(DIVIDER);
         scanner.close();
