f0cdf9ef1b321fba20af27f5f9fbedf4829bd330
Shiverin <shiverinz@gmail.com>
2026-08-19 17:11:47 +0800

    Add task completion tracking

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -14,7 +14,7 @@ public class Nimbus {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         System.out.println(DIVIDER);
         System.out.println("Hello! I'm Nimbus.");
         System.out.println("What can I do for you?");
@@ -29,8 +29,20 @@ public class Nimbus {
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i + 1) + ". " + tasks.get(i));
                 }
+            } else if (command.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(command.substring(5));
+                Task task = tasks.get(taskNumber - 1);
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task);
+            } else if (command.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(command.substring(7));
+                Task task = tasks.get(taskNumber - 1);
+                task.markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + task);
             } else {
-                tasks.add(command);
+                tasks.add(new Task(command));
                 System.out.println("Added: " + command);
             }
             System.out.println(DIVIDER);
---
+++ src/main/java/Task.java
@@ -0,0 +1,36 @@
+/**
+ * Represents a task and its completion status.
+ */
+public class Task {
+    protected final String description;
+    private boolean isDone;
+
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description Description of the task.
+     */
+    public Task(String description) {
+        this.description = description;
+    }
+
+    /** Marks this task as completed. */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /** Marks this task as incomplete. */
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    /** Returns the icon that represents this task's completion status. */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
